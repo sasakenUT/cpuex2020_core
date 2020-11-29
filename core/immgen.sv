1955ab0f1df50d0df0d1c62481cfa4daf4e30ba8
sasaken <sasaken@LAPTOP-D00V3DBB.localdomain>
2020-11-30 00:46:37 +0900

    Simulation, Synsthesis, and Implementation have passed successfully

--- core/immgen.sv
+++ core/immgen.sv
@@ -11,9 +11,9 @@ module immgen(input wire logic [31:0] instr,
     case (op)
       7'b0010011: imm = {{20{instr[31]}}, instr[31:20]};  // OP-IMM
       7'b1100011: imm = {{19{instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0};  // OP-BRANCH
-      7'b0110111: imm = {{12{instr[31]}}, instr[31:12]};  // LUI
-      7'b0010111: imm = {{12{instr[31]}}, instr[31:12]};  // AUIPC
-      7'b1101111: imm = {{11{instr[31]}}, instr[20], instr[19:12], instr[20], instr[30:21], 1'b0};  // JAL
+      7'b0110111: imm = {instr[31:12], 12'b0};  // LUI
+      7'b0010111: imm = {instr[31:12], 12'b0};  // AUIPC
+      7'b1101111: imm = {{11{instr[31]}}, instr[31], instr[19:12], instr[20], instr[30:21], 1'b0};  // JAL
       7'b1100111: imm = {{20{instr[31]}}, instr[31:20]};  // JALR
       7'b0000011: imm = {{20{instr[31]}}, instr[31:20]};  // LW
       7'b0100011: imm = {{20{instr[31]}}, instr[31:25], instr[11:7]}; // SW
@@ -21,4 +21,5 @@ module immgen(input wire logic [31:0] instr,
     endcase
 
 endmodule
+
 `default_nettype wire
