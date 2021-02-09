73227d553c16e8a1e672cba7aed1a24487c9b3db
MyDesktop <sasaken@MyComputer.localdomain>
2021-02-09 19:41:54 +0900

    added uart unit for server to top_tb

--- test/tb/test_top_tb.sv
+++ test/tb/test_top_tb.sv
@@ -6,7 +6,17 @@ module test_top_tb();
   // Input output signals
   logic clk, rstn, txd, rxd;
 
+  // Internal signals
+  logic [7:0] indata, outdata;
+  logic       tx_start, tx_busy, rready, ferr;
+
+  // uart parameter
+  localparam CLK_PER_HALF_BIT = 434;
+
+  // Instanciate units
   top uut(clk, rstn, txd, rxd);
+  uart_tx #(CLK_PER_HALF_BIT) serv_tx(indata, tx_start, tx_busy, rxd, clk, rstn);   // server -> top
+  uart_rx #(CLK_PER_HALF_BIT) serv_rx(outdata, rready, ferr, txd, clk, rstn);       // server <- top
 
   // generate clock
   always
@@ -20,9 +30,14 @@ module test_top_tb();
   initial
   begin
     rstn <= 0;
-    rxd  <= 0;
     #100;
+    tx_start <= 0;
     rstn <= 1;
+    indata <= 8'ha;
+    #10;
+    tx_start <= 1;
+    #10;
+    tx_start <= 0;
   end
 
 endmodule
